d1c31648c30de472ebb4631570da665812e1a1b9
school <bano.notit@mail.ru>
2018-11-13 14:57:37 +0300

    Пытаемся сделать логику бота-хайзенберга

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ module.exports = class Bot {
         console.log('Bot created')
 
         this.api = api
+        this.inDialogWith = []
     }
 
 
@@ -25,7 +26,13 @@ module.exports = class Bot {
 
                 switch (messageType) {
                     case 'message_changed': {
-                        return this.handleEvent({channel: event.channel, ...event.message})
+                        return this.handleEvent(
+                            Object.assign({
+                                channel: event.channel
+                            }
+                            , event.message
+                            )
+                            )
                     }
 
                     case 'normal': {
@@ -36,15 +43,40 @@ module.exports = class Bot {
                         console.log(event)
                         console.log(`${event.user} in ${event.channel_type}: ${event.text}`)
 
-                        if (event.text.toLowerCase().includes('kenobi')) {
+                        if (
+                            event.text.toLowerCase().includes('who the hell are you') && 
+                                !this.inDialogWith.includes(event.user)
+                        ) {
+			    this.inDialogWith.push(event.user)
+
                             this.api
                                 .sendMessage({
                                     channel: event.channel
-                                    , text: 'Hi there!'
+                                    , text: 'You know. You all know exactly who I am. Say my name.'
                                 })
                                 .then(res => {
                                     console.log(res)
                                 })
+                        } else if (
+                            event.text.toLowerCase().includes('heizenberg') && 
+                                this.inDialogWith.includes(event.user)
+                        ) {
+			   const i = this.inDialogWith.indexOf(this.user)
+			   this.inDialogWith.splice(i, 1)
+
+			   this.api
+			       .sendMessage({
+			           channel: event.channel
+				   , text: 'You\'re goddamn right!'
+			       })
+                        } else if (this.inDialogWith.includes(event.user)) {
+
+                            this.api
+                                .sendMessage({
+                                    channel: event.channel
+                                    , text: 'No, you know my name. I\'m the cook. I\'m the man who killed Gus Fring. Now. Say my name.'
+                                })
+
                         }
                         break
                     }
